f8b40ba2f49597fc82c8ec1d6d67de7806f71496
Rene Elias <reneelias03@gmail.com>
2025-07-13 12:56:05 -0700

    Boat prefab + basic controls + slight lake changes

---
+++ Assets/Scripts/Boat.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Boat : MonoBehaviour
+{
+    [SerializeField] protected float speed = 5.0f;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        MovementControls();
+    }
+
+    void MovementControls()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.AddForce(Vector2.up * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddForce(Vector2.down * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(Vector2.left * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(Vector2.right * speed * Time.deltaTime);
+        }
+    }
+}
