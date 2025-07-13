0d6f0232d0dd978de9f62c78566717f48f6070d4
foolishroutine <37634500+foolishroutine@users.noreply.github.com>
2025-07-13 11:58:56 -0700

    Scripts and Prefab folder

---
+++ Assets/Scripts/RazziSpawner.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RazziSpawner : MonoBehaviour
+{
+    public Transform RazziPrefab;
+    public float SpawnRate = 2f;
+
+
+    void Update()
+    {
+    }
+}
