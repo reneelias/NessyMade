5a34d346ea9a257925b568fc04482ff9fb8d23c8
Rene Elias <reneelias03@gmail.com>
2025-07-13 18:11:32 -0700

    BoatTrash and BoatFish update and follow player

---
+++ Assets/Scripts/BoatFish.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatFish : MonoBehaviour
+{
+    [SerializeField] private Sprite[] sprites;
+    SpriteRenderer spriteRenderer;
+    [SerializeField] private Player player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    /// <summary>
+    /// This function is called every fixed framerate frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void FixedUpdate()
+    {
+        transform.position = player.transform.position;
+        spriteRenderer.sortingOrder = player.GetComponent<SpriteRenderer>().sortingOrder;
+    }
+
+    public void SetAmount(int amt)
+    {
+        if (amt > 0)
+        {
+            spriteRenderer.enabled = true;
+            spriteRenderer.sprite = sprites[amt - 1];
+        }
+        else
+        {
+            spriteRenderer.enabled = false;
+        }
+    }
+
+    public void SetSpriteFlip(bool flip)
+    {
+        spriteRenderer.flipX = flip;
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,7 +7,7 @@ public class Player : MonoBehaviour
 {
     [SerializeField] protected float speed = 5.0f;
     private Rigidbody2D rb;
-    public enum PlayerState{Idle, FishCarry, TrashCarry, RazziCarry, FishFeed, TrashToss, RazziFeed}
+    public enum PlayerState { Idle, FishCarry, TrashCarry, RazziCarry, FishFeed, TrashToss, RazziFeed }
     public PlayerState playerState
     {
         protected set;
@@ -15,6 +15,8 @@ public class Player : MonoBehaviour
     } = PlayerState.Idle;
     private int trashAmount = 0;
     private int fishAmount = 0;
+    [SerializeField] protected BoatFish boatFish;
+    [SerializeField] protected BoatTrash boatTrash;
 
     // Start is called before the first frame update
     void Start()
@@ -45,8 +47,14 @@ public class Player : MonoBehaviour
             case PlayerState.TrashToss:
                 break;
         }
-        
+
         UpdateDirection();
+    }
+
+    void Update()
+    {
+        UpdateFishCount();
+        UpdateTrashCount();
     }
 
     void UpdateDirection()
@@ -54,6 +62,59 @@ public class Player : MonoBehaviour
         Vector3 scale = transform.localScale;
         scale.x = rb.velocity.x < 0 ? -Mathf.Abs(scale.x) : Mathf.Abs(scale.x);
         transform.localScale = scale;
+        boatFish.SetSpriteFlip(rb.velocity.x < 0);
+        boatTrash.SetSpriteFlip(rb.velocity.x < 0);
+    }
+
+    void UpdateFishCount()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if (--fishAmount <= 0)
+            {
+                fishAmount = 0;
+                SetPlayerState(PlayerState.Idle);
+            }
+            boatFish.SetAmount(fishAmount);
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if (++fishAmount > 4)
+            {
+                fishAmount = 4;
+            }
+            if (playerState != PlayerState.FishCarry)
+            {
+                SetPlayerState(PlayerState.Idle);
+            }
+
+            boatFish.SetAmount(fishAmount);
+        }
+    }
+    void UpdateTrashCount()
+    {
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if (--trashAmount <= 0)
+            {
+                trashAmount = 0;
+                SetPlayerState(PlayerState.Idle);
+            }
+            boatTrash.SetAmount(trashAmount);
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (++trashAmount > 4)
+            {
+                trashAmount = 4;
+            }
+            if (playerState != PlayerState.TrashCarry)
+            {
+                SetPlayerState(PlayerState.Idle);
+            }
+
+            boatTrash.SetAmount(trashAmount);
+        }
     }
 
     void MovementControls()
@@ -74,5 +135,9 @@ public class Player : MonoBehaviour
         {
             rb.AddForce(Vector2.right * speed * Time.deltaTime);
         }
+    }
+
+    public void SetPlayerState(PlayerState ps) {
+        playerState = ps;
     }
 }
