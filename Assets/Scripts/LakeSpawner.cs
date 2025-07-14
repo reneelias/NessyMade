fba968ae81ab539b9035340952cd7a32d22c4947
foolishroutine <37634500+foolishroutine@users.noreply.github.com>
2025-07-13 19:44:20 -0700

    LakeSpawner for trash and fish

---
+++ Assets/Scripts/LakeSpawner.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class LakeSpawner : MonoBehaviour
+{
+
+    public Transform SpawnPrefab;
+    public KeyCode InputKey;
+    public Transform CenterSpawnPoint;
+    
+    [Range(0f, 3f)] public float DistanceMin;
+    [Range(0f, 3f)] public float DistanceMax;
+    
+    public bool AutoSpawn;
+    public float SpawnRate = 2f;
+    private float spawnTimer;
+    
+    
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(InputKey))
+        {
+            spawn();
+        }
+
+        if (!AutoSpawn) return;
+        spawnTimer += Time.deltaTime;
+        if (spawnTimer >= SpawnRate)
+        {
+            spawnTimer -= (SpawnRate + Random.Range(0f, 1f));
+            spawn();
+        }
+    }
+
+    private void spawn()
+    {
+        var direction = Random.insideUnitCircle.normalized;
+        
+        var distance = Random.Range(DistanceMin, DistanceMax);
+        var spawnPoint = (Vector2)CenterSpawnPoint.position + distance * direction;
+        var newSpawn = Instantiate(SpawnPrefab, CenterSpawnPoint , false);
+        newSpawn.localPosition = spawnPoint;
+    }
+}
